946e119d5271ed831fa4a1607d4f0840c3992ac0
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-03-17 15:29:30 +0000

    Completed POST/redirect/Get refactor
    Co-Authored-By: Lou Reade <39278690+loushark@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -1,19 +1,28 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
     'Testing infrastructure working!'
   end
 
   get '/battle' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
     erb(:index)
   end
 
   post '/names' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
-    erb(:play)
+    session[:name1] = params[:name1]
+    session[:name2] = params[:name2]
+    redirect '/play'
   end
+
+  get '/play' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
+    erb :play
+  end
+
+  run! if app_file == $0
 end
